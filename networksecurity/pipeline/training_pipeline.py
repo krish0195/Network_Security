158b2be39a89f2ea506a31e8b65f3c5eac33fb5c
krishnaraj <krish0195@gmail.com>
2024-09-03 01:25:10 +0530

    utils is updated

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -117,11 +117,13 @@ class TrainingPipeline:
             TrainingPipeline.is_pipeline_running=True
             
             data_ingestion_artifact=self.start_data_ingestion()
-            print(data_ingestion_artifact)
+            #print(data_ingestion_artifact)
             
             data_validation_artifact=self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
-            print(data_validation_artifact)
+            #print(data_validation_artifact)
             
+            data_transformation_artifact=self.start_data_transformation(data_validation_artifact=data_validation_artifact)
+            print(data_transformation_artifact)
             
         except Exception as e:
             raise  NetworkSecurityException(e,sys)
